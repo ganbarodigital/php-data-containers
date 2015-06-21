bdb7c27a117330f626f9db8a1ab8354674359fca
Stuart Herbert <stuart@stuartherbert.com>
2015-06-21 18:39:25 +0100

    New container: LazyValueObject

---
+++ src/LazyValueObject.php
@@ -0,0 +1,125 @@
+<?php
+
+/**
+ * Copyright (c) 2015-present Ganbaro Digital Ltd
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *
+ *   * Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in
+ *     the documentation and/or other materials provided with the
+ *     distribution.
+ *
+ *   * Neither the names of the copyright holders nor the names of his
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * @category  Libraries
+ * @package   DataContainers
+ * @author    Stuart Herbert <stuherbert@ganbarodigital.com>
+ * @copyright 2015-present Ganbaro Digital Ltd www.ganbarodigital.com
+ * @license   http://www.opensource.org/licenses/bsd-license.php  BSD License
+ * @link      http://code.ganbarodigital.com/php-factfinder
+ */
+
+namespace GanbaroDigital\DataContainers;
+
+use JsonSerializable;
+use RuntimeException;
+
+class LazyValueObject extends BaseContainer implements JsonSerializable
+{
+	/** @var array a list of fake operations supported */
+	protected $supportedOperations = [
+		'get'   => 'getData',
+		'has'   => 'hasData',
+		'set'   => 'setData',
+		'reset' => 'resetData',
+	];
+
+	/**
+	 * make this value object read-only
+	 *
+	 * @return void
+	 */
+	public function makeReadOnly()
+	{
+		unset($this->supportedOperations['set']);
+		unset($this->supportedOperations['reset']);
+	}
+
+	/**
+	 * magic method, which provides fake getter / setting support
+	 *
+	 * @param  string $methodName
+	 *         the fake method that was called
+	 * @param  array $args
+	 *         a list of the args that were passed to us
+	 * @return mixed
+	 *         depends on the fake method being called
+	 */
+	public function __call($methodName, $args)
+	{
+		list($verb, $infoName) = $this->convertMethodName($methodName);
+
+		if (!isset($this->supportedOperations[$verb])) {
+			throw new E4xx_NoSuchMethod(get_class($this), $methodName);
+		}
+
+		$callable   = [ $this, $verb . 'Data'];
+		$argsToPass = array_merge([ $infoName ], $args);
+		return call_user_func_array($callable, $argsToPass);
+	}
+
+	/**
+	 * given the 'get/set' kind of method name, convert it into:
+	 *
+	 * 1. the operation being performed (get, set, et al)
+	 * 2. a normalised version of the name of the information
+	 *
+	 * @param  string $methodName the method name to decode
+	 * @return array<string>
+	 */
+	protected function convertMethodName($methodName)
+	{
+        // turn the method name into an array of words
+        $words = explode(' ', preg_replace('/([^A-Z])([A-Z])/', "$1 $2", $methodName));
+
+        // lose the first word
+        $verb = array_shift($words);
+
+        // concat into underscore_format
+        $retval = implode("_", $words);
+
+        // all done
+        return [$verb, $retval];
+	}
+
+	/**
+	 * returns the data to turn into a JSON data structure
+	 * @return object
+	 */
+	public function jsonSerialize()
+	{
+		return (object)$this->getAllData();
+	}
+}
---
+++ tests/LazyValueObjectTest.php
@@ -0,0 +1,228 @@
+<?php
+
+/**
+ * Copyright (c) 2015-present Ganbaro Digital Ltd
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *
+ *   * Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in
+ *     the documentation and/or other materials provided with the
+ *     distribution.
+ *
+ *   * Neither the names of the copyright holders nor the names of his
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * @category  Libraries
+ * @package   GanbaroDigital/DataContainers
+ * @author    Stuart Herbert <stuherbert@ganbarodigital.com>
+ * @copyright 2015-present Ganbaro Digital Ltd www.ganbarodigital.com
+ * @license   http://www.opensource.org/licenses/bsd-license.php  BSD License
+ * @link      http://code.ganbarodigital.com/php-factfinder
+ */
+
+namespace GanbaroDigital\DataContainers;
+
+use PHPUnit_Framework_TestCase;
+
+/**
+ * @coversDefaultClass GanbaroDigital\DataContainers\LazyValueObject
+ */
+class LazyValueObjectTest extends PHPUnit_Framework_TestCase
+{
+	/**
+	 * @coversNone
+	 */
+	public function testCanInstantiate()
+	{
+	    // ----------------------------------------------------------------
+	    // setup your test
+
+	    $obj = new LazyValueObject;
+
+	    // ----------------------------------------------------------------
+	    // test the results
+
+	    $this->assertTrue($obj instanceof LazyValueObject);
+	}
+
+	/**
+	 * @covers ::__call
+	 * @covers ::convertMethodName
+	 * @dataProvider getAndSetDataProvider
+	 */
+	public function testCanGetAndSetData($dataName, $expectedData)
+	{
+	    // ----------------------------------------------------------------
+	    // setup your test
+
+	    $obj = new LazyValueObject;
+
+	    // ----------------------------------------------------------------
+	    // perform the change
+
+	    $obj->{'set' . $dataName }($expectedData);
+
+	    // ----------------------------------------------------------------
+	    // test the results
+
+	    $actualData = $obj->{'get' . $dataName }();
+	    $this->assertEquals($expectedData, $actualData);
+	}
+
+	public function getAndSetDataProvider()
+	{
+		return [
+			[ 'Value1', true ],
+			[ 'Value2', false ]
+		];
+	}
+
+	/**
+	 * @covers ::__call
+	 * @dataProvider getAndSetDataProvider
+	 */
+	public function testCanCheckDataExists($dataName, $expectedData)
+	{
+	    // ----------------------------------------------------------------
+	    // setup your test
+
+	    $obj = new LazyValueObject;
+
+	    // ----------------------------------------------------------------
+	    // perform the change
+
+	    $actualData = $obj->{'has' . $dataName}();
+	    $this->assertFalse($actualData);
+	    $obj->{'set' . $dataName}($expectedData);
+
+	    // ----------------------------------------------------------------
+	    // test the results
+
+	    $actualData = $obj->{'has' . $dataName}();
+	    $this->assertTrue($actualData);
+	}
+
+	/**
+	 * @covers ::__call
+	 * @dataProvider getAndSetDataProvider
+	 */
+	public function testCanRemoveData($dataName, $expectedData)
+	{
+	    // ----------------------------------------------------------------
+	    // setup your test
+
+	    $obj = new LazyValueObject;
+
+	    $actualData = $obj->{'has' . $dataName}();
+	    $this->assertFalse($actualData);
+	    $obj->{'set' . $dataName}($expectedData);
+
+	    $actualData = $obj->{'has' . $dataName}();
+	    $this->assertTrue($actualData);
+
+	    // ----------------------------------------------------------------
+	    // perform the change
+
+	    $obj->{'reset' . $dataName}();
+
+	    // ----------------------------------------------------------------
+	    // test the results
+
+	    $actualData = $obj->{'has' . $dataName}();
+	    $this->assertFalse($actualData);
+	}
+
+	/**
+	 * @covers ::__call
+	 * @expectedException GanbaroDigital\DataContainers\E4xx_NoSuchMethod
+	 */
+	public function testThrowsExceptionWhenUnsupportedMethodCalled()
+	{
+	    // ----------------------------------------------------------------
+	    // setup your test
+
+	    $obj = new LazyValueObject;
+
+	    // ----------------------------------------------------------------
+	    // perform the change
+
+	    $obj->doSomethingWeirdAndWonderful();
+	}
+
+	/**
+	 * @covers ::__call
+	 * @covers ::makeReadOnly
+	 * @dataProvider getAndSetDataProvider
+	 * @expectedException GanbaroDigital\DataContainers\E4xx_NoSuchMethod
+	 */
+	public function testCanMakeReadOnly($dataName, $expectedData)
+	{
+	    // ----------------------------------------------------------------
+	    // setup your test
+
+	    $obj = new LazyValueObject;
+	    $obj->{'set' . $dataName }($expectedData);
+
+	    // ----------------------------------------------------------------
+	    // perform the change
+
+	    $obj->makeReadOnly();
+
+	    // ----------------------------------------------------------------
+	    // test the results
+
+	    // this will trigger the exception
+	    $obj->{'set' . $dataName }($expectedData);
+	}
+
+	/**
+	 * @covers ::jsonSerialize
+	 */
+	public function testCanJsonEncode()
+	{
+	    // ----------------------------------------------------------------
+	    // setup your test
+
+	    $obj = new LazyValueObject;
+
+	    $rawData = [
+	    	"Value1" => 100,
+	    	"Value2" => 200,
+	    ];
+	    $expectedData = json_encode($rawData);
+
+	    // ----------------------------------------------------------------
+	    // perform the change
+
+	    foreach ($rawData as $key => $value) {
+		    $obj->{'set' . $key}($value);
+		}
+
+	    // ----------------------------------------------------------------
+	    // test the results
+
+	    $actualData = json_encode($obj);
+	    $this->assertEquals($expectedData, $actualData);
+	}
+}
