30cbee4eab6f8f66016017292fe6a3534070d248
Stuart Herbert <stuart@stuartherbert.com>
2015-07-14 08:01:25 +0100

    WIP: DescendDotNotationPath value builder

---
+++ src/ValueBuilders/DescendDotNotationPath.php
@@ -0,0 +1,261 @@
+<?php
+
+/**
+ * Copyright (c) 2015-present Ganbaro Digital Ltd
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *
+ *   * Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in
+ *     the documentation and/or other materials provided with the
+ *     distribution.
+ *
+ *   * Neither the names of the copyright holders nor the names of his
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * @category  Libraries
+ * @package   DataContainers/ValueBuilders
+ * @author    Stuart Herbert <stuherbert@ganbarodigital.com>
+ * @copyright 2011-present Mediasift Ltd www.datasift.com
+ * @copyright 2015-present Ganbaro Digital Ltd www.ganbarodigital.com
+ * @license   http://www.opensource.org/licenses/bsd-license.php  BSD License
+ * @link      http://code.ganbarodigital.com/php-data-containers
+ */
+
+namespace GanbaroDigital\DataContainers\ValueBuilders;
+
+use GanbaroDigital\DataContainers\Exceptions\E4xx_NoSuchIndex;
+use GanbaroDigital\DataContainers\Exceptions\E4xx_NoSuchProperty;
+use GanbaroDigital\DataContainers\Exceptions\E4xx_UnsupportedType;
+use GanbaroDigital\Reflection\Checks\IsAssignable;
+use GanbaroDigital\Reflection\Checks\IsTraversable;
+use GanbaroDigital\Reflection\ValueBuilders\FirstMethodMatchingType;
+use GanbaroDigital\Reflection\ValueBuilders\SimpleType;
+
+class DescendDotNotationPath
+{
+    /**
+     * descend inside an array, using dot.notation.support, and optionally
+     * extending the array if the end of the dot.notation.path is missing
+     *
+     * @param  array &$arr
+     *         the array to dig into
+     * @param  string $index
+     *         the dot.notation.support path to descend
+     * @param  array|callable|string|null $extendingItem
+     *         if we need to extend, what data type do we extend using?
+     * @return mixed
+     */
+    public static function intoArray(&$arr, $index, $extendingItem = null)
+    {
+        try {
+            return self::getPathFromRoot($arr, $index, $extendingItem);
+        }
+        catch (E4xx_NoSuchIndex $e) {
+            throw new E4xx_NoSuchIndex($arr, $index);
+        }
+        catch (E4xx_NoSuchProperty $e) {
+            throw new E4xx_NoSuchIndex($arr, $index);
+        }
+    }
+
+    /**
+     * descend inside an object, using dot.notation.support, and optionally
+     * extending the object if the end of the dot.notation.path is missing
+     *
+     * @param  object $obj
+     *         the object to dig into
+     * @param  string $property
+     *         the dot.notation.support path to descend
+     * @param  array|callable|string|null $extendingItem
+     *         if we need to extend, what data type do we extend using?
+     * @return mixed
+     */
+    public static function intoObject($obj, $property, $extendingItem = null)
+    {
+        try {
+            return self::getPathFromRoot($obj, $property, $extendingItem);
+        }
+        catch (E4xx_NoSuchIndex $e) {
+            throw new E4xx_NoSuchProperty($obj, $property);
+        }
+        catch (E4xx_NoSuchProperty $e) {
+            throw new E4xx_NoSuchProperty($obj, $property);
+        }
+    }
+
+    /**
+     * descend inside a container, using dot.notation.support, and optionally
+     * extending the container if the end of the dot.notation.path is missing
+     *
+     * @param  mixed $item
+     *         the container to dig into
+     * @param  string $property
+     *         the dot.notation.support path to descend
+     * @param  array|callable|string|null $extendingItem
+     *         if we need to extend, what data type do we extend using?
+     * @return mixed
+     */
+    public static function intoMixed($item, $property, $extendingItem = null)
+    {
+        if (IsAssignable::checkMixed($item)) {
+            return self::intoObject($item, $property, $extendingItem);
+        }
+        if (IsTraversable::checkMixed($item)) {
+            return self::intoArray($item, $property, $extendingItem);
+        }
+
+        throw new E4xx_UnsupportedType(SimpleType::fromMixed($item));
+    }
+
+    /**
+     * descend inside a variable, using dot.notation.support, and optionally
+     * extending the variable if the end of the dot.notation.path is missing
+     *
+     * @param  object|array $item
+     *         the variable to dig into
+     * @param  string $path
+     *         the dot.notation.support path to descend
+     * @param  array|callable|string|null $extendingItem
+     *         if we need to extend, what data type do we extend using?
+     * @return mixed
+     */
+    public function __invoke($item, $path, $extendingItem = null)
+    {
+        $methodName = FirstMethodMatchingType::fromMixed($item, get_class($this), 'into');
+        return self::$methodName($item, $path, $extendingItem);
+    }
+
+    /**
+     * get whatever is at the end of the given dot.notation.support path,
+     * optionally extending the path as required
+     *
+     * @param  array|object $root
+     *         where we start from
+     * @param  string $path
+     *         the dot.notation.support path to descend
+     * @param  array|callable|string|null $extendingItem
+     *         if we need to extend, what data type do we extend using?
+     * @return mixed
+     */
+    private static function &getPathFromRoot($root, $path, $extendingItem)
+    {
+        $parts = explode(".", $path);
+        $retval = $root;
+        foreach ($parts as $part) {
+            if (is_object($retval)) {
+                $retval = self::getPartFromObject($retval, $part, $extendingItem);
+            }
+            else if (is_array($retval)) {
+                $retval = self::getPartFromArray($retval, $part, $extendingItem);
+            }
+            else if ($extendingItem !== null) {
+                throw new E4xx_CannotExtendPath($root, $path, $part);
+            }
+            else {
+                throw new E4xx_NoSuchProperty($root, $path);
+            }
+        }
+
+        // if we get here, then we have found what they are looking for
+        return $retval;
+    }
+
+    /**
+     * get a property from an object, extending the object if the property
+     * does not exist
+     *
+     * @param  object $obj
+     *         the object to look inside
+     * @param  string $part
+     *         the name of the property to extract
+     * @param  array|callable|string|null $extendingItem
+     *         if we need to extend, what data type do we extend using?
+     * @return mixed
+     */
+    private static function &getPartFromObject($obj, $part, $extendingItem = null)
+    {
+        // general case
+        if (isset($obj->$part)) {
+            return $obj->$part;
+        }
+
+        // can we extend?
+        if ($extendingItem === null) {
+            throw new E4xx_NoSuchProperty($obj, $part);
+        }
+
+        $obj->$part = self::getExtension($extendingItem);
+        return $obj->$part;
+    }
+
+    /**
+     * get an index from an array, extending the array if the index does
+     * not exist
+     *
+     * @param  array &$arr
+     *         the array to look inside
+     * @param  string $part
+     *         the index to look up
+     * @param  array|callable|string|null $extendingItem
+     *         if we need to extend, what data type do we extend using?
+     * @return mixed
+     */
+    private static function &getPartFromArray(&$arr, $part, $extendingItem = null)
+    {
+        if (isset($arr[$part])) {
+            return $arr[$part];
+        }
+
+        // can we extend?
+        if ($extendingItem === null) {
+            throw new E4xx_NoSuchIndex('array', $part);
+        }
+
+        $arr[$part] = self::getExtension($extendingItem);
+        return $arr[$part];
+    }
+
+    /**
+     * create new extension
+     *
+     * @param  array|callable|string|null $extendingItem
+     *         what data type do we extend using?
+     * @return array|object
+     *         the extension that has been created
+     */
+    private static function getExtension($extendingItem)
+    {
+        if (is_array($extendingItem)) {
+            return $extendingItem;
+        }
+
+        // if they don't return anything useful, that is their problem!
+        if (is_callable($extendingItem)) {
+            return $extendingItem();
+        }
+
+        // assume that it is a class name
+        return new $extendingItem;
+    }
+}
---
+++ tests/ValueBuilders/DescendDotNotationPathTest.php
@@ -0,0 +1,374 @@
+<?php
+
+/**
+ * Copyright (c) 2015-present Ganbaro Digital Ltd
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *
+ *   * Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in
+ *     the documentation and/or other materials provided with the
+ *     distribution.
+ *
+ *   * Neither the names of the copyright holders nor the names of his
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * @category  Libraries
+ * @package   DataContainers/ValueBuilders
+ * @author    Stuart Herbert <stuherbert@ganbarodigital.com>
+ * @copyright 2015-present Ganbaro Digital Ltd www.ganbarodigital.com
+ * @license   http://www.opensource.org/licenses/bsd-license.php  BSD License
+ * @link      http://code.ganbarodigital.com/php-data-containers
+ */
+
+namespace GanbaroDigital\DataContainers\ValueBuilders;
+
+use GanbaroDigital\UnitTestHelpers\ClassesAndObjects\InvokeMethod;
+use PHPUnit_Framework_TestCase;
+use stdClass;
+
+/**
+ * @coversDefaultClass GanbaroDigital\DataContainers\ValueBuilders\DescendDotNotationPath
+ */
+class DescendDotNotationPathTest extends PHPUnit_Framework_TestCase
+{
+    /**
+     * @coversNothing
+     */
+    public function testCanInstantiate()
+    {
+        // ----------------------------------------------------------------
+        // setup your test
+
+        // ----------------------------------------------------------------
+        // perform the change
+
+        $obj = new DescendDotNotationPath;
+
+        // ----------------------------------------------------------------
+        // test the results
+
+        $this->assertTrue($obj instanceof DescendDotNotationPath);
+    }
+
+    /**
+     * @covers ::__invoke
+     * @dataProvider provideContainersAndPaths
+     */
+    public function testCanUseAsObject($container, $path, $expectedResult)
+    {
+        // ----------------------------------------------------------------
+        // setup your test
+
+        $obj = new DescendDotNotationPath;
+
+        // ----------------------------------------------------------------
+        // perform the change
+
+        $actualResult = $obj($container, $path);
+
+        // ----------------------------------------------------------------
+        // test the results
+
+        $this->assertEquals($expectedResult, $actualResult);
+    }
+
+    /**
+     * @covers ::__invoke
+     * @dataProvider provideNonContainers
+     * @expectedException GanbaroDigital\DataContainers\Exceptions\E4xx_UnsupportedType
+     */
+    public function testThrowsExceptionIfCalledAsObjectWithNonContainer($container)
+    {
+        // ----------------------------------------------------------------
+        // setup your test
+
+        $obj = new DescendDotNotationPath();
+
+        // ----------------------------------------------------------------
+        // perform the change
+
+        $obj($container, 'one.two');
+    }
+
+    /**
+     * @covers ::intoMixed
+     * @dataProvider provideContainersAndPaths
+     */
+    public function testCanCallStatically($container, $path, $expectedResult)
+    {
+        // ----------------------------------------------------------------
+        // setup your test
+
+        // ----------------------------------------------------------------
+        // perform the change
+
+        $actualResult = DescendDotNotationPath::intoMixed($container, $path);
+
+        // ----------------------------------------------------------------
+        // test the results
+
+        $this->assertEquals($expectedResult, $actualResult);
+    }
+
+    /**
+     * @covers ::intoMixed
+     * @dataProvider provideNonContainers
+     * @expectedException GanbaroDigital\DataContainers\Exceptions\E4xx_UnsupportedType
+     */
+    public function testThrowsExceptionIfCalledStaticallyWithNonContainer($container)
+    {
+        // ----------------------------------------------------------------
+        // setup your test
+
+
+
+        // ----------------------------------------------------------------
+        // perform the change
+
+        DescendDotNotationPath::intoMixed($container, 'one.two');
+    }
+
+    /**
+     * @covers ::intoArray
+     * @dataProvider provideArrayContainersAndPaths
+     */
+    public function testCanStaticallyCallIntoArray($container, $path, $expectedResult)
+    {
+        // ----------------------------------------------------------------
+        // setup your test
+
+        // ----------------------------------------------------------------
+        // perform the change
+
+        $actualResult = DescendDotNotationPath::intoArray($container, $path);
+
+        // ----------------------------------------------------------------
+        // test the results
+
+        $this->assertEquals($expectedResult, $actualResult);
+    }
+
+    /**
+     * @covers ::intoObject
+     * @dataProvider provideObjectContainersAndPaths
+     */
+    public function testCanStaticallyCallIntoObject($container, $path, $expectedResult)
+    {
+        // ----------------------------------------------------------------
+        // setup your test
+
+        // ----------------------------------------------------------------
+        // perform the change
+
+        $actualResult = DescendDotNotationPath::intoObject($container, $path);
+
+        // ----------------------------------------------------------------
+        // test the results
+
+        $this->assertEquals($expectedResult, $actualResult);
+    }
+
+    /**
+     * @covers ::getPartFromObject
+     */
+    public function testCanTraverseObjects()
+    {
+        // ----------------------------------------------------------------
+        // setup your test
+
+        $obj = (object)[ "one" => 1, "two" => 2];
+        $expectedResult = 1;
+
+        // ----------------------------------------------------------------
+        // perform the change
+
+        $actualResult = InvokeMethod::onClass(DescendDotNotationPath::class, 'getPartFromObject', [ $obj, "one" ]);
+
+        // ----------------------------------------------------------------
+        // test the results
+
+        $this->assertEquals($expectedResult, $actualResult);
+    }
+
+    /**
+     * @covers ::getPartFromObject
+     * @expectedException GanbaroDigital\DataContainers\Exceptions\E4xx_NoSuchProperty
+     */
+    public function testThrowsExceptionWhenObjectDoesNotContainPart()
+    {
+        // ----------------------------------------------------------------
+        // setup your test
+
+        $obj = (object)[ "one" => 1, "two" => 2];
+
+        // ----------------------------------------------------------------
+        // perform the change
+
+        $actualResult = InvokeMethod::onClass(DescendDotNotationPath::class, 'getPartFromObject', [ $obj, "three" ]);
+    }
+
+    /**
+     * @covers ::getPartFromObject
+     * @covers ::getExtension
+     */
+    public function testCanExtendObjectUsingArray()
+    {
+        // ----------------------------------------------------------------
+        // setup your test
+
+        $obj = (object)[ "one" => 1, "two" => 2];
+        $expectedResult = [];
+
+        // ----------------------------------------------------------------
+        // perform the change
+
+        $actualResult = InvokeMethod::onClass(
+            DescendDotNotationPath::class,
+            'getPartFromObject',
+            [ $obj, "three", [] ]
+        );
+
+        // ----------------------------------------------------------------
+        // test the results
+
+        $this->assertEquals($expectedResult, $actualResult);
+        $this->assertTrue(isset($obj->three));
+        $this->assertTrue(is_array($obj->three));
+    }
+
+    /**
+     * @covers ::getPartFromObject
+     * @covers ::getExtension
+     */
+    public function testCanExtendObjectUsingCallable()
+    {
+        // ----------------------------------------------------------------
+        // setup your test
+
+        $obj = (object)[ "one" => 1, "two" => 2];
+        $callable = function() {
+            return "extended!";
+        };
+
+        $expectedResult = $callable();
+
+        // ----------------------------------------------------------------
+        // perform the change
+
+        $actualResult = InvokeMethod::onClass(
+            DescendDotNotationPath::class,
+            'getPartFromObject',
+            [ $obj, "three", $callable ]
+        );
+
+        // ----------------------------------------------------------------
+        // test the results
+
+        $this->assertEquals($expectedResult, $actualResult);
+        $this->assertTrue(isset($obj->three));
+        $this->assertEquals($expectedResult, $obj->three);
+    }
+
+    /**
+     * @covers ::getPartFromObject
+     * @covers ::getExtension
+     */
+    public function testCanExtendObjectUsingClassname()
+    {
+        // ----------------------------------------------------------------
+        // setup your test
+
+        $obj = (object)[ "one" => 1, "two" => 2];
+        $expectedResult = new stdClass;
+
+        // ----------------------------------------------------------------
+        // perform the change
+
+        $actualResult = InvokeMethod::onClass(
+            DescendDotNotationPath::class,
+            'getPartFromObject',
+            [ $obj, "three", stdClass::class ]
+        );
+
+        // ----------------------------------------------------------------
+        // test the results
+
+        $this->assertEquals($expectedResult, $actualResult);
+        $this->assertTrue(isset($obj->three));
+        $this->assertTrue($obj->three instanceof stdClass);
+    }
+
+    public function provideContainersAndPaths()
+    {
+        return array_merge(
+            $this->provideArrayContainersAndPaths(),
+            $this->provideObjectContainersAndPaths()
+        );
+    }
+
+    public function provideArrayContainersAndPaths()
+    {
+        $arrContainer = [
+            "one" => 1,
+            "two" => 2,
+            "three" => [ 1, 2, 3 => [ 4, 5, 6 ]]
+        ];
+        return [
+            [
+                $arrContainer,
+                "one",
+                1
+            ]
+        ];
+    }
+
+    public function provideObjectContainersAndPaths()
+    {
+        $objContainer = (object)[
+            "one" => 1,
+            "two" => 2,
+            "three" => [ 1, 2, 3 => [ 4, 5, 6 ]]
+        ];
+        return [
+            [
+                $objContainer,
+                "one",
+                1
+            ]
+        ];
+    }
+
+    public function provideNonContainers()
+    {
+        return [
+            [ null ],
+            [ false ],
+            [ true ],
+            [ 3.1415927 ],
+            [ 100 ],
+            [ fopen("php://input", "r") ],
+            [ "hello, world!" ]
+        ];
+    }
+}
